ea3d9739786f868ab27c0f9efe3a37ec14eb0799
TheLastStyleBender25 <90109978+TheLastStyleBender25@users.noreply.github.com>
2026-08-21 02:28:44 +0530

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CustomerData(BaseModel):
-    credit_score: int
-    age: int
-    tenure: int
-    balance: float
-    num_of_products: int
-    has_cr_card: int
-    is_active_member: int
-    estimated_salary: float
+    credit_score: int = Field(..., ge=300, le=850)
+    age: int = Field(..., ge=18)
+    tenure: int = Field(..., ge=0, le=10)
+    balance: float = Field(..., ge=0)
+    num_of_products: int = Field(..., ge=1)
+    has_cr_card: int = Field(..., ge=0, le=1)
+    is_active_member: int = Field(..., ge=0, le=1)
+    estimated_salary: float = Field(..., ge=0)
     geography: str
     gender: str
 
